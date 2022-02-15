d24a66a7de7e1f57595e278213e05b16295f1f9a
pavece <pavece04@gmail.com>
2022-02-15 20:13:57 +0100

    display no elements component added

---
+++ urlShort-frontend/src/components/noElements.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NoElements() {
+	return (
+		<div className="no-elements-container">
+			<img src="no-elements-ilu.svg" alt="" className="no-elements-ilu" />
+			<h1>Add something to start</h1>
+		</div>
+	);
+}
+
+export default NoElements;
