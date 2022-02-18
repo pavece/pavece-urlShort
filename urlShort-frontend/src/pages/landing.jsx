dd5ceebeb2b0df52f47e3af62321a207fb4c11dd
pavece <pavece04@gmail.com>
2022-02-18 23:45:10 +0100

    removed features section

--- urlShort-frontend/src/pages/landing.jsx
+++ urlShort-frontend/src/pages/landing.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Nav from "../components/nav";
 import LandingHeroSection from "../components/landingHeroSection";
-import LandingFeaturesSection from "../components/landingFeaturesSection";
 import LandingFooter from "../components/landingFooter";
 export const Landing = () => {
 	return (
 		<div>
 			<Nav />
 			<LandingHeroSection />
-			<LandingFeaturesSection />
 			<LandingFooter />
 		</div>
 	);
